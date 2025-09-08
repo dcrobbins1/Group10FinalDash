a8c04ded2470b0cdd86de849077fc50880c1114a
dcrobbins1 <dcrobbins@wm.edu>
2025-09-08 16:03:59 -0400

    home edits

--- FinalMultiplePages/pages/home.py
+++ FinalMultiplePages/pages/home.py
@@ -1,1 +1,12 @@
 # home page
+
+import dash
+from dash import html
+
+dash.register_page(__name__, path="/")
+
+layout = html.Div([
+    html.H2("Welcome to our Home Page"),
+    html.P("We will add more info later")
+    
+])
