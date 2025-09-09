3055afb0f1fc9a4f37367dbb2aac02cde9375b73
Redeemer Gawu <rsgawu@wm.edu>
2025-09-09 14:38:35 -0400

    Update home.py

--- FinalMultiplePages/pages/home.py
+++ FinalMultiplePages/pages/home.py
@@ -5,15 +5,14 @@ from dash import html
 
 dash.register_page(__name__, path="/", name="Home")
 
-layout = html.Div([
-    html.H2("Welcome to the Food & Drinks Explorer"),
-    html.P([
-    "Are you looking for a New Recipe?",
-    html.Br(),
-    "Are you Thirsty?",
-    html.Br(),
-    "You have come to the Right Place!.",
-]),
-html.P("Use the navigation above to Explore!."),
-    
-])
+layout = html.Div(
+    [
+        html.H2("Welcome to FOOD & DRINKS EXPLORER"),
+        html.P("Are you looking for a New Recipe?"),
+        html.P("Are you Thirsty?"),
+        html.P("You have come to the Right Place!"),
+        html.P("Use the navigation above to Explore."),
+    ],
+    id="home"  
+)
+
