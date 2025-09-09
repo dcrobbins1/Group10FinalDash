4454dc2cfbe54988075f269fed93ea79bbb84369
dcrobbins1 <dcrobbins@wm.edu>
2025-09-09 13:02:29 -0400

    app test

--- FinalMultiplePages/app.py
+++ FinalMultiplePages/app.py
@@ -6,15 +6,28 @@ from dash import html, dcc
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
 
+#app.layout = dbc.Container([
+#    html.H1("Food & Drinks Explorer", className="text-center my-3"),
+#    dbc.Nav(
+##            dbc.NavLink("Home", href="/", active="exact"),
+  #          dbc.NavLink("About", href="/about",active="exact"),
+  #          dbc.NavLink("Recipes", href="/recipes",active="exact"),
+  #          dbc.NavLink("Brewery", href="/brewery",active="exact"),
+   #         for page in dash.page_registry.values()
+   #     ],
+   #     pills=True,
+   #     justified=True,
+    #    className="mb-4"
+    #),
+ #   dash.page_container
+#], fluid=True)
+
 app.layout = dbc.Container([
     html.H1("Food & Drinks Explorer", className="text-center my-3"),
     dbc.Nav(
         [
             dbc.NavLink("Home", href="/", active="exact"),
             dbc.NavLink("About", href="/about",active="exact"),
-            dbc.NavLink("Recipes", href="/recipes",active="exact"),
-            dbc.NavLink("Brewery", href="/brewery",active="exact"),
-            for page in dash.page_registry.values()
         ],
         pills=True,
         justified=True,
@@ -22,6 +35,8 @@ app.layout = dbc.Container([
     ),
     dash.page_container
 ], fluid=True)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
